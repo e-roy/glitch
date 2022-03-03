ea837503d6a784681b02508d8dd58d172a60976a
ropats16 <rohitcpathare@gmail.com>
2022-03-03 10:26:36 +0530

    updated dashboard card icons

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -4,6 +4,9 @@ import { withIronSessionSsr } from "iron-session/next";
 import { ironOptions } from "../../lib/session";
 // import { AppHeader } from "../../components/layout";
 import { Logout } from "../../components/wallet";
+import Image  from 'next/image'
+import videocam from "./video-cam.png";
+import streamimg from "./stream-img.png";
 
 const DashboardPage: NextPage = () => {
   return (
@@ -17,17 +20,32 @@ const DashboardPage: NextPage = () => {
         <Logout/>
       </header>
       <div className="m-4 flex justify-center">
-        <div className="bg-backgroundLight h-96 w-1/5 flex items-end">
+      
+        <div className="bg-backgroundLight w-1/5 grid place-items-center p-10 rounded-xl">
+          <Image
+            src={videocam}
+            width={150}
+            height={150}
+            alt="Video Cam"
+            className="rounded-lg"
+          />
           <Link href="/viewer">
-            <button className="font-bold px-10 py-2 mx-auto mb-20 border hover:text-secondary border-secondary rounded hover:bg-backgroundLight bg-secondary text-backgroundDark">
+            <button className="font-bold h-12 px-10 py-2 mx-auto mt-10 mb-10 border hover:text-secondary border-secondary rounded hover:bg-backgroundLight bg-secondary text-backgroundDark">
               View Stream
             </button>
           </Link>
         </div>
         
-        <div className="bg-backgroundLight h-96 w-1/5 flex ml-20 items-end">
+        <div className="bg-backgroundLight w-1/5 grid place-items-center ml-20 p-10 rounded-xl">
+          <Image
+            src={streamimg}
+            width={150}
+            height={150}
+            alt="Stream Img"
+            className="rounded-lg"
+          />
           <Link href="/creator">
-            <button className="font-bold px-10 py-2 mx-auto mb-20 border hover:text-secondary border-secondary rounded hover:bg-backgroundLight bg-secondary text-backgroundDark">
+            <button className="font-bold px-10 py-2 mx-auto mt-10 mb-10 border hover:text-secondary border-secondary rounded hover:bg-backgroundLight bg-secondary text-backgroundDark">
               Create Stream
             </button>
           </Link>
