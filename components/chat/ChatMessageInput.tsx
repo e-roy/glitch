1de761036e4fc830df81134f2f608657dc25cc1b
Diego Alzate <diego35.da@gmail.com>
2022-03-05 14:39:19 -0500

    styles: changed hover interaction in chatMessageinput

--- components/chat/ChatMessageInput.tsx
+++ components/chat/ChatMessageInput.tsx
@@ -1,12 +1,12 @@
 export const ChatMessageInput = () => {
   return (
-    <div className="flex rounded bg-transparent text-backgroundLight hover:text-blue-400">
+    <div className="flex rounded bg-transparent text-backgroundLight">
       <input
         type="text"
         placeholder="message"
-        className="bg-backgroundDark/50 rounded text-white border-none focus-none w-full p-2"
+        className="bg-backgroundDark/50 rounded text-white border-none focus-none w-full p-2 outline-none focus-visible:outline-secondary"
       />
-      <button className="font-black mx-2 p-4 w-20 border border-secondary rounded-lg bg-secondary hover:bg-backgroundLight ">
+      <button className="font-black mx-2 p-4 w-20 border border-secondary rounded-lg bg-secondary hover:bg-backgroundLight hover:text-secondary ">
         Send
       </button>
     </div>
