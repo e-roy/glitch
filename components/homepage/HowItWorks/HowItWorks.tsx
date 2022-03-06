1bbc4c19a6488a70f98020556dde942ed0166731
e-roy <ericroupe@gmail.com>
2022-03-06 15:15:32 -0500

    cleaned up video control

--- components/homepage/HowItWorks/HowItWorks.tsx
+++ components/homepage/HowItWorks/HowItWorks.tsx
@@ -7,13 +7,15 @@ import giveRec from "./giveRec.png";
 import ellipse from "./Ellipse.png";
 
 export const HowItWorks = () => (
-  <div className="py-32 px-32 mt-14 mr-16 mb-14 ml-16 flex flex-col space-y-7 rounded overflow-hidden" id="how-it-works">
-
+  <div
+    className="py-32 px-32 mt-14 mr-16 mb-14 ml-16 flex flex-col space-y-7 rounded overflow-hidden"
+    id="how-it-works"
+  >
     <div className="self-center ">
-      <div className=" bg-[#D53D5C] w-20 h-20 -mb-16 -ml-5 rounded-full">
-
-      </div>
-      <h3 className=" text-secondary font-oxanium font-bold text-xl md:text-6xl self-center z-4">How It Works</h3>
+      <div className=" bg-[#D53D5C] w-20 h-20 -mb-16 -ml-5 rounded-full"></div>
+      <h3 className=" text-secondary font-oxanium font-bold text-xl md:text-6xl self-center z-4">
+        How It Works
+      </h3>
     </div>
 
     <div className="flex self-center">
@@ -25,28 +27,9 @@ export const HowItWorks = () => (
           alt="Wallet Icon"
           className="rounded-lg"
         />
-        <p className="text-secondary justify-center py-5">Connect Your Wallet</p>
-      </div>
-
-      <div className="arrowIcon py-3.5">
-        <Image
-          src={dashLine}
-          width={70}
-          height={0}
-          alt="Arrow Icon"
-          className="rounded-lg"
-        />
-      </div>
-
-      <div className="paste px-3.5">
-        <Image
-          src={paste}
-          width={70}
-          height={70}
-          alt="Paste Icon"
-          className="rounded-lg"
-        />
-        <p className="text-secondary py-5">Paste Livepeer API</p>
+        <p className="text-secondary justify-center py-5">
+          Connect Your Wallet
+        </p>
       </div>
 
       <div className="arrowIcon py-3.5">
@@ -91,6 +74,5 @@ export const HowItWorks = () => (
         <p className="text-secondary py-5">Give/Receive $POST</p>
       </div>
     </div>
-
   </div>
 );
