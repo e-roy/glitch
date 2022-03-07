ee55a741c3f15d44ece62e9aab145dd175519f86
Diego Alzate <diego35.da@gmail.com>
2022-03-06 20:15:18 -0500

    feat: send message with enter key

--- components/chat/ChatMessageInput.tsx
+++ components/chat/ChatMessageInput.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useState, KeyboardEvent } from "react";
 
 type ChatMessageInputProps = {
   onClick: (message: string) => void
@@ -15,6 +15,13 @@ export const ChatMessageInput = ({onClick}: ChatMessageInputProps) => {
     onClick(message)
     setMessage('')
   }
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => { 
+    if (e.code === "Enter") {
+      handleClick()
+    }
+  }
+
   return (
     <div className="flex rounded bg-transparent text-backgroundLight">
       <input
@@ -23,6 +30,7 @@ export const ChatMessageInput = ({onClick}: ChatMessageInputProps) => {
         value={message}
         className="bg-backgroundDark/50 rounded text-white border-none focus-none w-full p-2 outline-none focus-visible:outline-secondary"
         onChange={handleChange}
+        onKeyDown={handleKeyDown}
       />
       <button type="button" onClick={handleClick} className="font-black mx-2 p-4 w-20 border border-secondary rounded-lg bg-secondary hover:bg-backgroundLight hover:text-secondary ">
         Send
